ef2b0c38ced6e13178508d5dd3326bc62b684e4f
khalifa47 <khalifafumo5@gmail.com>
2024-04-16 13:38:55 +0300

    feat: create GET routes
    Closes #4

---
+++ controllers/cats.controller.js
@@ -0,0 +1,37 @@
+let cats = [
+    { id: 1, name: 'Flippy', age: 2 },
+    { id: 2, name: 'Misty', age: 5 },
+    { id: 3, name: 'Tigger', age: 1 },
+    { id: 4, name: 'Whiskers', age: 3 }
+]; // [{ id, name, age }]
+
+const meow = (name) => {
+    return `<h1>${name} says meow!</h1>`;
+};
+
+exports.getAllCats = async (req, res) => {
+    try {
+        let message = '';
+        cats.forEach(cat => {
+            message += meow(cat.name);
+        });
+
+        res.send(message)
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+exports.getCatById = async (req, res) => {
+    try {
+        const cat = cats.find(cat => cat.id === parseInt(req.params.id));
+        if (cat) {
+            res.send(meow(cat.name));
+        } else {
+            res.status(404).json({ message: 'Cat not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000;
+const catsRouter = require('./routes/cats.router');
+
+app.use(express.json());
+
+app.use('/cats', catsRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
